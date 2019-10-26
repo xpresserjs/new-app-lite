6375d88bf8911be9d5c47a396218603d390141f4
Evil <lordcashtugbaski@gmail.com>
2019-10-26 01:55:41 +0100

    Updated Xpresser

--- config.js
+++ config.js
@@ -5,7 +5,25 @@
 module.exports = {
     // name of app
     name: "Xpresser-Simple-App",
+    /**
+     * By default xjs sets this for you.
+     */
+    server: {
+        domain: 'localhost',
+        // Server Port
+        port: 3000,
+    },
+
+    /**
+     * Path settings.
+     */
     paths: {
+        /**
+         * Base Folder
+         * Where this app is called from.
+         *
+         * Best value for this is: __dirname
+         */
         base: __dirname,
     }
 };
